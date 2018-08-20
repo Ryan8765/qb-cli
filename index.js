3a0264d3fac0c8db64ee2a886ea914b45bd33ac8
Ryan Haas <rhaas@mcftech.com>
2018-08-20 18:40:45 -0500

    Updated to include Realm in inputs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 //npm
-
 const clear = require('clear');
 const userInput  = require('./lib/userInput');
 const Configstore = require('configstore');
@@ -18,10 +17,10 @@ const helpers = require('./lib/helpers');
 //init configstore
 const conf = new Configstore(pkg.name);
 
-
+//configs
 const qbCLIConfName = 'qbcli.json';
 const extensionPrefix = 'MCF_';
-const extensionPrefixDev = 'MCF_d';
+const extensionPrefixDev = 'MCF_d_';
 
 
 
@@ -58,6 +57,9 @@ const run = async () => {
 
     //if running the deploy
     } else if (args._.includes('deploy')) {
+        //running a spinner
+        // const status = new Spinner('Authenticating you, please wait...');
+        // status.start();  
 
         //make sure user is running this from the root of their react directory
         if (!files.fileFolderExists(`${qbCLIConfName}`)) {
@@ -66,27 +68,63 @@ const run = async () => {
         } 
 
         //make sure build folder exists
-        if( !files.fileFolderExists('./build') ) console.log(chalk.red('This qbcli command can only be run from the root of your directory.  You must install qbcli before using it, and your react app must be built (npm run build).'));
+        if( !files.fileFolderExists('./build') ) {
+            console.log(chalk.red('This qbcli command can only be run from the root of your directory.  You must install qbcli before using it, and your react app must be built (npm run build).'));
+            return;
+        }
 
         //get filenames in the build folder
         const fileNamesInBuild = files.getFilesFromDirectory('./build');
 
-        if( !fileNamesInBuild ) console.log(chalk.red('You may have installed the qbcli in the wrong directory.  Please reinstall in the top level directory of your React application.'));
+        if( !fileNamesInBuild ) {
+            console.log(chalk.red('You may have installed the qbcli in the wrong directory.  Please reinstall in the top level directory of your React application.'));
+            return;
+        }
 
         const { repositoryId } = files.readJSONFile(`./${qbCLIConfName}`);
 
-        //get configs
+        //get configs stored from qbcli install
         const configs = conf.get( repositoryId );
 
-        if( !configs ) console.log(chalk.red('Project may never have been initialized - please run qbcli install.'));
+        if( !configs ) {
+            console.log(chalk.red('Project may never have been initialized - please run qbcli install.'));
+            return;
+        }
 
+        //get filenames in the build folder
         const cssFileName = helpers.getFileNameFromExt_h(fileNamesInBuild, '.css');
         const jsFileName = helpers.getFileNameFromExt_h(fileNamesInBuild, '.js');
 
-        if( !cssFileName || !jsFileName ) console.log(chalk.red('Make sure you have run npm run build for your React application'));
+        if( !cssFileName || !jsFileName ) {
+            console.log(chalk.red('Make sure you have run npm run build for your React application'));
+            return;
+        }
+
+        var qbCSSFileName = null;
+        var qbJSFileName = null;
+        //create the filenames to be used for QB.
+        if( args._.includes('prod') ) {
+            qbCSSFileName = `${extensionPrefix}${repositoryId}_index.css`;
+            qbJSFileName = `${extensionPrefix}${repositoryId}_index.js`;
+        } else if( args._.includes('dev') ) {
+            qbCSSFileName = `${extensionPrefixDev}${repositoryId}_index.css`;
+            qbJSFileName = `${extensionPrefixDev}${repositoryId}_index.js`;
+        } else {
+            console.log(chalk.green('Please specify dev or prod deployment.'));
+            return;
+        }
+        console.log( qbCSSFileName );
+        console.log( qbJSFileName );
+
+
+        //get file contents from the build folder
+        var cssFileContents = files.getFileContents(`./build/${cssFileName}`);
+        var jsFileContents = files.getFileContents(`./build/${jsFileName}`);
 
 
 
+
+        // status.stop();
     }
 
     
